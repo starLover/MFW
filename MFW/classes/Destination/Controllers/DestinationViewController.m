9a851942470a9dfd38fde29cfe219d9840e47e29
马娟娟 <2891529590@qq.com>
2016-03-17 21:57:28 +0800

    mjj

--- MFW/classes/Destination/Controllers/DestinationViewController.m
+++ MFW/classes/Destination/Controllers/DestinationViewController.m
@@ -9,9 +9,12 @@
 #import "DestinationViewController.h"
 #import <AFNetworking/AFHTTPSessionManager.h>
 #import "DestinationTableViewCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface DestinationViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property(nonatomic,strong)UIView *tableViewHeaderView;
+@property(nonatomic,strong)NSMutableArray *albumArray;
 
 @end
 
@@ -20,11 +23,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //设置导航栏为全透明，且去掉边框黑线
+    [self.navigationController.navigationBar setTranslucent:YES];
+    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
+    //去黑线
+    self.navigationController.navigationBar.shadowImage = [[UIImage alloc] init];
+    
+    
+    [self configTableViewHeaderView];
     [self requestModel];
     
     
 }
 #pragma mark --------- UITableViewDataSource
+- (void)configTableViewHeaderView{
+    self.tableViewHeaderView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight/4*3-40)];
+    self.tableViewHeaderView.backgroundColor = [UIColor cyanColor];
+    
+    UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight/4*3/2)];
+//    [imageView.image ]
+    [self.tableViewHeaderView addSubview:imageView];
+    
+    self.tableView.tableHeaderView = self.tableViewHeaderView;
+}
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     DestinationTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     if (cell == nil) {
@@ -46,13 +67,13 @@
 #pragma mark --------- 网络请求
 - (void)requestModel{
     AFHTTPSessionManager *sessionManager = [AFHTTPSessionManager manager];
-    sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"text/html",@"application/json",nil];
     [sessionManager GET:kDestination parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
         
     } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSLog(@"%@",responseObject);
+
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"%@",error);
+        
     }];
 }
 
