92a0e0e98fff55b8cabf90b1dd694e559f8af8fe
聂欣欣 <1498146887@qq.com>
2016-03-17 18:56:31 +0800

    aaa

--- MFW/classes/Main/Controllers/MainViewController.m
+++ MFW/classes/Main/Controllers/MainViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "MainViewController.h"
+#import <UIImageView+WebCache.h>
 
-@interface MainViewController ()
-
+@interface MainViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property(nonatomic, strong) UITableView *tableView;
 @end
 
 @implementation MainViewController
@@ -17,7 +18,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+    [self.view addSubview:self.tableView];
+    [self configHeaderView];
 }
+
+#pragma mark    ---------------    UITableViewDataSource
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellIdentifier"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    return cell;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+#pragma mark    ---------------    UITableViewDelegate
+
+
+
+
+#pragma mark   -----------------          自定义table头部
+
+- (void)configHeaderView{
+    
+    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenWidth / 2)];
+    UIButton *headBtn = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenWidth / 2)];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenWidth / 2)];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:kHeaderList]];
+    headBtn.backgroundColor = [UIColor redColor];
+    [headBtn addTarget:self action:@selector(adAction) forControlEvents:UIControlEventTouchUpInside];
+    [headView addSubview:headBtn];
+    [headView addSubview:imageView];
+    self.tableView.tableHeaderView = headView;
+}
+- (void)adAction{
+    
+}
+#pragma mark      ---------- LazyLoading
+- (UITableView *)tableView{
+    if (_tableView == nil) {
+        self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 22, kScreenWidth, kScreenHeight - 44)];
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+        self.tableView.rowHeight = 120;
+    }
+    return _tableView;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ MFW/library/HWTools/MainDefine.h
@@ -0,0 +1,14 @@
+//
+//  MainDefine.h
+//  MFW
+//
+//  Created by wanghongxiao on 16/3/17.
+//  Copyright © 2016年 聂欣欣. All rights reserved.
+//
+
+#ifndef MainDefine_h
+#define MainDefine_h
+
+#define kHeaderList @"http://file32.mafengwo.net/M00/BC/EF/wKgBs1bhIjGAIXVOAADIp_DO9qk95.jpeg"
+
+#endif /* MainDefine_h */
