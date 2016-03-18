e1eb93b62fab0adcd3841e4c02a5d498f256bad2
聂欣欣 <1498146887@qq.com>
2016-03-18 21:59:22 +0800

    ss

---
+++ MFW/classes/Main/Views/SalesCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  SalesCollectionViewCell.h
+//  MFW
+//
+//  Created by wanghongxiao on 16/3/18.
+//  Copyright © 2016年 李志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MainModel.h"
+
+@interface SalesCollectionViewCell : UICollectionViewCell
+@property(nonatomic, strong) MainModel *mainModel;
+@end
