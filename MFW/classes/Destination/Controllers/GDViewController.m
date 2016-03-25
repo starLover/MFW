9d0108a3bc8a53a9fe110bc80b72f68a8963bd86
MaJuanJuan <2891529590@qq.com>
2016-03-25 17:22:02 +0800

    mjj

--- MFW/classes/Destination/Controllers/GDViewController.m
+++ MFW/classes/Destination/Controllers/GDViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "GDViewController.h"
+#import <MAMapKit/MAMapKit.h>
 
-@interface GDViewController ()
-
+@interface GDViewController ()<MAMapViewDelegate>
+{
+    MAMapView *_mapView;
+}
 @end
 
 @implementation GDViewController
@@ -17,8 +20,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self showBackBtn];
+    [MAMapServices sharedServices].apiKey = kGDKey;
+    _mapView = [[MAMapView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)];
+    _mapView.delegate = self;
+    [self.view addSubview:_mapView];
+    //定位,YES为打开定位，NO为关闭定位
+    _mapView.showsUserLocation = YES;
+    [self.view.superview addSubview:_mapView];
+    //地图跟着位置移动
+    [_mapView setUserTrackingMode:MAUserTrackingModeFollow animated:YES];
+    [_mapView setZoomLevel:16.1 animated:YES];
 }
-
+//- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation{
+//    if (updatingLocation) {
+//        NSLog(@"latitude:%f,lng:%f",userLocation.coordinate.latitude,userLocation.coordinate.longitude);
+//    }
+//}
+- (void)mapView:(MAMapView *)mapView didAddAnnotationViews:(NSArray *)views{
+    MAAnnotationView *view = views[0];
+    // 放到该方法中用以保证userlocation的annotationView已经添加到地图上了。
+    if ([view.annotation isKindOfClass:[MAUserLocation class]]) {
+        MAUserLocationRepresentation *pre = [[MAUserLocationRepresentation alloc]init];
+        pre.fillColor = [UIColor colorWithRed:0.9 green:0.1 blue:0.1 alpha:0.3];
+        pre.strokeColor = [UIColor colorWithRed:0.1 green:0.1 blue:0.9 alpha:1.0];
+        pre.image = [UIImage imageNamed:@"ic_topbar_location"];
+        pre.lineWidth = 3;
+        pre.lineDashPattern = @[@6,@3];
+        [_mapView updateUserLocationRepresentation:pre];
+        view.calloutOffset = CGPointMake(0, 0);
+    }
+}
+- (void)viewDidAppear:(BOOL)animated{
+    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc]init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(39.989631, 116.48);
+    pointAnnotation.title = @"洛阳";
+    pointAnnotation.subtitle = @"龙门";
+    [_mapView addAnnotation:pointAnnotation];
+}
+- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation{
+    if ([annotation isKindOfClass:[MAPointAnnotation class]]) {
+        static NSString *pointReuseIndentifier = @"";
+        MAPinAnnotationView *annotationView = (MAPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:pointReuseIndentifier];
+        if (annotationView == nil) {
+            annotationView = [[MAPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:pointReuseIndentifier];
+        }
+        annotationView.canShowCallout= YES;  //设置气泡可以弹出，默认为NO
+        annotationView.animatesDrop = YES; //设置标注动画显示，默认为NO
+        annotationView.draggable = YES;  //设置标注可以拖动，默认为NO
+        annotationView.pinColor = MAPinAnnotationColorPurple;
+        return annotationView;
+    }
+    return nil;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
